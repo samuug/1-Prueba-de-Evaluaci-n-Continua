211e6c436af813376817a9dd69a7b64d9ea3a0ce
samuug <113547223+samuug@users.noreply.github.com>
2023-03-23 20:19:25 +0100

    Clase Main actualizada
    Método de la clase main actualizado para el ejercicio 1 y 2

--- _1_Prueba_de_Evaluacion/src/main/java/org/example/Main.java
+++ _1_Prueba_de_Evaluacion/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.Ejercicio1.AproximacionPi;
+import org.example.Ejercicio2.Tablero;
 
 
 import java.util.Scanner;
@@ -12,11 +13,24 @@ public class Main {
         int opcion;
         do {
             System.out.println("1.- Práctica 1. Obtención de una aproximación al número pi");
+            System.out.println("2.- Práctica 2. El juego de la vida");
             System.out.println("4.- Salir");
             opcion = sc.nextInt();
-            switch (opcion) {
+            switch (opcion){
                 case 1:
-                    System.out.println("El valor de pi es: " + AproximacionPi.generarNumeroPi(AproximacionPi.generarPasos()));
+                    System.out.println("El valor de pi es: " + AproximacionPi.generarNumeroPi(1000000));
+                    break;
+                case 2:
+                    Tablero tablero = new Tablero();
+                    tablero.generarEstadoActualPorMontecarlo();
+                    tablero.imprimirTablero();
+                    for (int i = 0; i < 15; i++) {
+                        System.out.println("Generación "+(i+1)+":");
+                        tablero.leerEstadoActual();
+                        tablero.transitarAlEstadoSiguiente();
+                        tablero.imprimirTablero();
+                        System.out.println();
+                    }
                     break;
                 case 4:
                     System.out.println("Saliendo...");
